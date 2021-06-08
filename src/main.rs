542760721724bad89faf9cc0a76ed9f6dea0442e
Callum Moseley <callum.moseley1@gmail.com>
2021-06-08 17:48:17 -0500

    basic wifi form

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+use rocket::request::Form;
+use rocket::response::{status, NamedFile, Redirect};
+use std::path::{Path, PathBuf};
+
 #[macro_use]
 extern crate rocket;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/main.html")).ok()
+}
+
+#[derive(FromForm)]
+struct WifiConfig {
+    ssid: String,
+    pw: String,
+}
+
+#[post("/connect", data = "<form>")]
+fn connect(form: Form<WifiConfig>) -> status::Accepted<String> {
+    println!("ssid: {} pw: {}", form.ssid, form.pw);
+    status::Accepted(Some("Connecting".to_string()))
+}
+
+#[get("/<path..>")]
+fn redirect(path: PathBuf) -> Redirect {
+    Redirect::found(uri!(index))
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index, connect, redirect])
+        .launch();
 }
