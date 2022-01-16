89e76ea6cd766c76c3f5bae19288fbb91219db13
Callum Moseley <callum.moseley1@gmail.com>
2022-01-16 02:18:09 -0500

    update iptables

--- iptables.sh
+++ iptables.sh
@@ -1,2 +1,2 @@
-iptables -t nat -A PREROUTING -p tcp -m tcp --dport 80 -j DNAT --to-destination 192.168.4.1:2050
-iptables -t nat -A PREROUTING -p tcp -m tcp --dport 443 -j DNAT --to-destination 192.168.4.1:2050
+iptables -t nat -A PREROUTING -i ap@wlan0 -p tcp -m tcp  -s 192.168.4.0/24 --dport 80 -j DNAT --to-destination 192.168.4.1:2050
+iptables -t nat -A PREROUTING -i ap@wlan0 -p tcp -m tcp  -s 192.168.4.0/24 --dport 443 -j DNAT --to-destination 192.168.4.1:2050
